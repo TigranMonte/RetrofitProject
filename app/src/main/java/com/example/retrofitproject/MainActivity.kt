83cd5dd0ce67283c0dfec6c60ecc3c9d62d1cea1
Tigran <tigranma@gmail.com>
2023-04-24 17:20:22 +0300

    Retrofit app (тестовый запрос)

--- app/src/main/java/com/example/retrofitproject/MainActivity.kt
+++ app/src/main/java/com/example/retrofitproject/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.retrofitproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import com.example.retrofitproject.retrofit.ProductApi
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv)
+        val b = findViewById<Button>(R.id.btnGet)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com")
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        b.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(3)
+                runOnUiThread {
+                    tv.text = product.title
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/retrofitproject/retrofit/ProductApi.kt
+++ app/src/main/java/com/example/retrofitproject/retrofit/ProductApi.kt
@@ -1,6 +1,9 @@
 package com.example.retrofitproject.retrofit
 
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ProductApi {
-
-
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Long): Product
 }
