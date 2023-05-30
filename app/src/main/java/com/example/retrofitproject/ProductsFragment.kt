04e8bafa9058e11e7ac513889b93acdb49933e8b
TigranMonte <tigranma@gmail.com>
2023-05-30 22:09:22 +0300

    Retrofit project update

--- app/src/main/java/com/example/retrofitproject/ProductsFragment.kt
+++ app/src/main/java/com/example/retrofitproject/ProductsFragment.kt
@@ -19,7 +19,6 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ProductsFragment : Fragment() {
-
     private lateinit var adapter: ProductAdapter
     private lateinit var binding: FragmentProductsBinding
     private lateinit var mainApi: MainApi
@@ -31,7 +30,6 @@ class ProductsFragment : Fragment() {
         binding = FragmentProductsBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRetrofit()
@@ -45,7 +43,6 @@ class ProductsFragment : Fragment() {
             }
         }
     }
-
     private fun initRetrofit(){
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -59,7 +56,6 @@ class ProductsFragment : Fragment() {
             .addConverterFactory(GsonConverterFactory.create()).build()
         mainApi = retrofit.create(MainApi::class.java)
     }
-
     private fun initRcView() = with(binding){
         adapter = ProductAdapter()
         rcView.layoutManager = LinearLayoutManager(context)
