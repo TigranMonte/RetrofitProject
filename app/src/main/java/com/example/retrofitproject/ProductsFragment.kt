79df4b79550ed60036dc98fce13c691deb9bd501
Tigran <tigranma@gmail.com>
2023-06-14 20:58:58 +0300

    Retrofit app - example app

--- app/src/main/java/com/example/retrofitproject/ProductsFragment.kt
+++ app/src/main/java/com/example/retrofitproject/ProductsFragment.kt
@@ -32,7 +32,6 @@ class ProductsFragment : Fragment() {
         binding = FragmentProductsBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initRetrofit()
@@ -46,7 +45,6 @@ class ProductsFragment : Fragment() {
             }
         }
     }
-
     private fun initRetrofit(){
         val interceptor = HttpLoggingInterceptor()
         interceptor.level = HttpLoggingInterceptor.Level.BODY
@@ -60,7 +58,6 @@ class ProductsFragment : Fragment() {
             .addConverterFactory(GsonConverterFactory.create()).build()
         mainApi = retrofit.create(MainApi::class.java)
     }
-
     private fun initRcView() = with(binding){
         adapter = ProductAdapter()
         rcView.layoutManager = LinearLayoutManager(context)
