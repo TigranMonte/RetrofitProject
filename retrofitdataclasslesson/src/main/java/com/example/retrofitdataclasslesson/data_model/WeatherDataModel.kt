a8d282759e0d3e0a0d3b9179b9602bf594c57d6b
TigranMonte <tigranma@gmail.com>
2023-07-21 23:28:45 +0300

    Retrofit project (weather info)

--- retrofitdataclasslesson/src/main/java/com/example/retrofitdataclasslesson/data_model/WeatherDataModel.kt
+++ retrofitdataclasslesson/src/main/java/com/example/retrofitdataclasslesson/data_model/WeatherDataModel.kt
@@ -5,15 +5,18 @@ data class WeatherDataModel(
     val current: CurrentModel,
     val forecast: ForecastModel
 )
+
 data class LocalModel(
     val name: String,
     val localtime : String
 )
+
 data class CurrentModel(
     val last_updated: String,
     val temp_c: String,
     val condition: ConditionModel
 )
+
 data class ConditionModel(
     val text: String,
     val icon: String
